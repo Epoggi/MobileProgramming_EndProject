e9cb664eb62244b8aad41159421be1673f27ef00
Epoggi <11327084+Epoggi@users.noreply.github.com>
2020-11-20 16:54:27 +0200

    ...

---
+++ Pages/Settings.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Alert, FlatList, TextInput } from 'react-native';
+import { styles }  from '../Blocks/Styles';
+
+export default function Settings() {
+
+  return (
+
+    <View style={styles.screen}>
+        <Text>Hello</Text>
+   </View>
+  );
+}
